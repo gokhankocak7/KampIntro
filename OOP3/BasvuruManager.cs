c539ef69c559378eec3f0e15c9e1316921d99d00
gokhankocak7 <gokhankocak7@gmail.com>
2021-01-23 23:51:50 +0300

    23 Ocak Ders Sonu

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //Method Injection
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService) 
+        {
+            //Başvuran bilgilerini değerlendirme
+            //
+             krediManager.Hesapla();
+             loggerService.Log();
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+
+        internal void BasvuruYap(IKrediManager konutKrediManager, object fileLoggerService)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void BasvuruYap(IKrediManager konutKrediManager, DatabaseLoggerService databaseLoggerService)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
